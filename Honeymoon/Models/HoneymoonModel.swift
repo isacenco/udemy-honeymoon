b001abc99277d7d2c82dc9853dccc2b835f087b8
Ghenadie Isacenco <isacenco@gmail.com>
2025-11-01 11:53:15 +0100

    Card View

---
+++ Honeymoon/Models/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by Ghenadie Isacenco on 01/11/2025.
+//
+
+import SwiftUI
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
---
+++ Honeymoon/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Honeymoon
+//
+//  Created by Ghenadie Isacenco on 01/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView(honeymoon: honeymoonData[1])
+    }
+}
+
+#Preview {
+    ContentView()
+}
